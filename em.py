dd305148a7ab4c4443fccfbada0ceb6376ee7393
suganya1098 <59464243+suganya1098@users.noreply.github.com>
2024-02-03 13:56:46 +0530

    Update em.py

--- em.py
+++ em.py
@@ -1,1 +1,9 @@
-i
+with open('deep_learning.jpg', 'rb') as f:
+                    img_data = f.read()
+                    image = MIMEImage(img_data, name='deep_learning')
+                    msg.attach(image)
+                smtp_session = smtplib.SMTP(smtp_server, smtp_port)
+                smtp_session.starttls()
+                smtp_session.login(smtp_username, smtp_password)
+                smtp_session.sendmail(smtp_username, receiving_email, msg.as_string())
+                smtp_session.quit()
